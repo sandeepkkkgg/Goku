6fcde7caa5f66b8a43d6640a13b5604ca44ba27d
sandeepkkkgg <sandeepkumar78812@gmail.com>
2025-02-11 14:40:57 +0530

    Index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = process.env.bot_token
-const id = process.env.bot_id
+const token = "7622570644:AAHVjda2jScbupVNzfqwczgAl39_bxG4y30"
+const id = "6810153416"
 const address = 'https://www.google.com'
 
 const app = express();
